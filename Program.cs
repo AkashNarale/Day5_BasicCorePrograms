094b39e5c0cb31803c75e78316c64dbd3645d610
AkashNarale <akashnarale43@gmail.com>
2022-10-15 15:36:38 +0530

    uc1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //flipCoin.HeadTail();
+            //leapYear.UC2();
+            //powerTwo.UC3();
+            //harmoniNo.UC4();
+            //factors.UC5();
+            //quotient.UC6();
+            //swapTwoNo.UC7();
+            //oddEven.UC8();
+            //vowelConsonant.UC9();
+            largestNumber.UC10();
+            Console.ReadLine();
+        }
+    }
+}
