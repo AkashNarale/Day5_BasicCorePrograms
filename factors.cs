525aa0c8b228531794dcc7aa727395e7e3a41b32
AkashNarale <akashnarale43@gmail.com>
2022-10-15 15:48:44 +0530

    uc10

---
+++ factors.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day5_Assignment
+{
+    internal class factors
+    {
+        //find prime factors of a number
+        public static void UC5()
+        {
+            //taking input from user
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            //local Variable
+            int isPrime;
+            for (int i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = 1;
+                    for (int j = 2; j <= i / 2; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = 0;
+                            break;
+                        }
+                    }
+                    if (isPrime == 1)
+                    {
+                        Console.WriteLine("Prime Factor is :" + i);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ leapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Assignment
+{
+    internal class leapYear
+    {
+        public static void UC2()
+        {
+            Console.WriteLine("Enter a year :");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year > 999 && year < 10000)
+            {
+                if (((year % 400) == 0) || ((year % 4) == 0) && ((year % 100) != 0))
+                {
+                    Console.WriteLine("{0} is a leap year", +year);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a leap year", +year);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter four digit year.");
+            }
+        }
+    }
+}
